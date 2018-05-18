eff367198cbe9f58574673dc3d0643d6c619e9da
gksthf2845 <gksthf2845@naver.com>
2018-05-18 22:54:18 +0900

    Update

---
+++ New Unity Project 1/Assets/BarTiming.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BarTiming : MonoBehaviour
+{
+    public Image FillTexture;
+    public Image Handler;
+    public Text text;
+
+    [Range(0, 1)]
+    public float fillrange = 1; //허용범위를 확인하려면 업데이트문으로 옮겨서 확인하세요
+
+    RectTransform RectTr;
+    RectTransform FiilRecrTr;
+    RectTransform HandleTr;
+
+
+    public float fiilrangeStart;
+    public float handlerMoveSpeed;
+
+    public float fillLimitValue;
+
+    private void Awake()
+    {
+        
+
+        RectTr = GetComponent<RectTransform>();
+
+        FiilRecrTr = FillTexture.GetComponent<RectTransform>();
+        HandleTr = Handler.GetComponent<RectTransform>();
+        //사이즈
+        RectTr.sizeDelta = new Vector2(RectTr.rect.width, RectTr.rect.height);
+        //허용범위의 사이즈
+
+
+        HandleTr.sizeDelta = new Vector2(HandleTr.rect.width, RectTr.rect.height);
+
+
+        RandomSet();
+
+        // RandomSet();
+    }
+    public float aa;
+    void Update()
+    {
+        aa =FiilRecrTr.localPosition.x;
+
+        FiilRecrTr.sizeDelta = new Vector2(Mathf.Clamp(FiilRecrTr.rect.width, 0, RectTr.rect.width - FiilRecrTr.localPosition.x), RectTr.rect.height);
+        //  fillLimitValue = RectTr.rect.width +  
+
+
+
+        //핸들 이동
+        HandleTr.transform.position = new Vector3(HandleTr.transform.position.x + handlerMoveSpeed, RectTr.transform.position.y, 0);
+            //실시간 범위값 변경
+           // FiilRecrTr.sizeDelta = new Vector2(Mathf.Clamp(RectTr.rect.width, RectTr.rect.width,)  , RectTr.rect.height);
+
+            if ((HandleTr.transform.position.x + HandleTr.rect.width) >= RectTr.transform.position.x + RectTr.rect.width)
+            {//핸들이 끝까지 갔을때
+                handlerMoveSpeed *= -1;
+            }
+
+            if (HandleTr.transform.position.x < RectTr.transform.position.x)
+            {//핸들이 한번  순회가 끝났을때
+            handlerMoveSpeed *= -1;
+            //this.gameObject.SetActive(false);
+            HandleTr.transform.position = new Vector3(RectTr.transform.position.x, RectTr.transform.position.y, -1);
+            }
+
+            //핸들이 범위에 들어왓을때.
+            if ((HandleTr.transform.position.x + HandleTr.rect.width) >= FiilRecrTr.transform.position.x &&
+               (HandleTr.transform.position.x + HandleTr.rect.width) <= FiilRecrTr.transform.position.x + FiilRecrTr.rect.width)
+            {
+            text.text = "in";
+            }
+            else
+            {
+            text.text = "out";
+
+        }
+    }
+    public void RandomSet()
+    {
+
+        FiilRecrTr.transform.position = new Vector3(0, RectTr.transform.position.y, -1);
+        HandleTr.transform.position = new Vector3(RectTr.transform.position.x, RectTr.transform.position.y, -1);
+
+        fiilrangeStart = Random.Range(0, RectTr.rect.width - RectTr.rect.width *0.1f ); //(,fillrange)
+
+        FiilRecrTr.localPosition = new Vector3(fiilrangeStart, FiilRecrTr.localPosition.y, 0);
+        //fill의 시작점 + 끝점 < rect의 끝점
+    }
+
+}
+
---
+++ New Unity Project 1/Assets/CircleTiming.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CircleTiming : MonoBehaviour {
+
+   
+    public float CurrentfillDegree;
+
+    public float currentCheckDegree;
+
+    public float spinSpeed=1;
+
+    public float FillRange;
+
+    public bool ClockWise = false;
+
+
+    [Range(0, 1)]
+    public float fillRangeMax = 1;
+
+    public bool isFillRamdom;
+
+    public RectTransform check;
+    public RectTransform fill;
+    public Image fillImage;
+    public Text text;
+
+    public float compatibleOffset=0; // 360  =1
+
+	void Awake () {
+
+        // check =GameObject.Find("FillobjectName").GetComponentInChildren<RectTransform>();
+        //  fill = GameObject.Find("checkobjectName").GetComponentInChildren<RectTransform>();
+        // fillImage = GameObject.Find("FillobjectName").GetComponent<Image>();
+
+        CurrentfillDegree = Random.Range(0, 360);
+
+        fill.rotation = Quaternion.Euler(0, 0, CurrentfillDegree);
+
+        check.rotation = Quaternion.Euler(0,0,0);
+
+        if (isFillRamdom) {
+            fillImage.fillAmount = Mathf.Clamp(Random.Range(0, fillRangeMax), 0, 1 - (CurrentfillDegree / 360));
+        }
+        else
+            fillImage.fillAmount = Mathf.Clamp(fillImage.fillAmount, 0, 1 - (CurrentfillDegree / 360)); 
+
+
+        //StartCoroutine("Clockwise");
+
+    }
+
+    void Update () {
+
+        compatibleOffset = fillImage.fillAmount * 360;
+
+        FillRange = Mathf.Clamp(CurrentfillDegree + compatibleOffset, 0, 360);
+
+
+        /*  if (CurrentfillDegree + compatibleOffset > 360)
+          {
+              FillRange = 360;
+
+              currentCheckDegree = Mathf.Repeat(currentCheckDegree, FillRange);
+          }
+          else
+          currentCheckDegree = Mathf.Repeat(currentCheckDegree,360 );
+     */
+        currentCheckDegree += spinSpeed ;
+        currentCheckDegree = Mathf.Repeat(currentCheckDegree,360);
+
+
+        check.rotation = Quaternion.Euler(check.rotation.x, check.rotation.y, currentCheckDegree);
+       
+        if ((currentCheckDegree >= CurrentfillDegree) && (currentCheckDegree <= FillRange))
+        {// 
+            text.text = "in";
+        }
+        else {
+            text.text = "out";
+        }
+
+	}
+
+    IEnumerator Clockwise ()
+    {
+        while (true) {
+            if (!ClockWise)
+            {
+                spinSpeed *= -1;
+                  yield return new WaitForSeconds(0.2f); 
+            }
+            else { spinSpeed = Mathf.Abs(spinSpeed);
+
+                yield return new WaitForSeconds(0.2f);
+            }
+        }
+        }
+     
+      
+}
